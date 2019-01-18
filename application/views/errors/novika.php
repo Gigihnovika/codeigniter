c997404ed623c49def42b37d88e8d98b5b73fe49
Gigihnovika <gigihvika24@gmail.com>
2019-01-18 21:23:11 +0700

    codeigniter

---
+++ application/views/errors/novika.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gigih</title>
+	<link rel="stylesheet" type="text/css" href="assets">
+</head>
+<body>
+ NAMA  	: GIGIH NOVIKA SURYANDARI
+ KELAS	: XI RPL 3
+ NO. URUT : 10
+ SMK TELKOM PURWOKERTO
+</body>
+</html>
